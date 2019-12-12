1a622921f73f5828c1779c1d0d4bfcc04870910c
th <huhuang03@gmail.com>
2019-12-12 15:23:49 +0800

    update

--- demo/src/main/java/com/th/thyi/demo/MainActivity.kt
+++ demo/src/main/java/com/th/thyi/demo/MainActivity.kt
@@ -3,7 +3,7 @@ package com.th.thyi.demo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.th.thyi.inner.ThyiEx
+import com.th.thyi.Thyi
 import com.th.thyi.demo.api.BaiduApi
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,13 +12,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val baiduApi = ThyiEx("http://www.baidu.com").create(BaiduApi::class.java)
-        baiduApi.home()
-                .map {
-                    tv.text = it
-                    Log.i("tonghu", "Thread; ${Thread.currentThread().name}")
-                    it
-                }
-                .subscribe({ }, { Log.i("tonghu", Log.getStackTraceString(it)) }, { Log.i("tonghu", "onFinish") })
+//        val baiduApi = Thyi("http://www.baidu.com").create(BaiduApi::class.java)
+//        baiduApi.home()
+//                .map {
+//                    tv.text = it
+//                    Log.i("tonghu", "Thread; ${Thread.currentThread().name}")
+//                    it
+//                }
+//                .subscribe({ }, { Log.i("tonghu", Log.getStackTraceString(it)) }, { Log.i("tonghu", "onFinish") })
     }
 }
--- thyi/src/main/java/com/th/thyi/inner/ApiMethod.kt
+++ thyi/src/main/java/com/th/thyi/inner/ApiMethod.kt
@@ -1,121 +1,122 @@
 package com.th.thyi.inner
-
-import android.util.Log
-import com.th.thyi.annotation.*
-import okhttp3.FormBody
-import okhttp3.Headers
-import okhttp3.Request
-import java.lang.IllegalStateException
-import java.lang.reflect.Method
-import java.lang.reflect.ParameterizedType
-import java.lang.reflect.Type
-import java.net.URL
-
-/**
- * api method为
- */
-data class ApiMethod(val method: Method, val args: (Array<Any?>)?) {
-    val returnType: Type = method.genericReturnType
-    val params: List<MethodParam> = method.parameterAnnotations.mapIndexed { index, annotations ->
-        MethodParam(annotations.toList(), (args!![index] as String?)
-                ?: "")
-    }
-    val annotations: List<Annotation> = method.annotations.toList()
-
-    init {
-        println(returnType)
-        require(returnType is ParameterizedType) { "method ${method.name} not return Observable" }
-    }
-
-
-    fun getHeaders(): List<MethodParam> {
-        return params.filter { it.hasAnnotation(HEADER::class.java) }
-    }
-
-    fun getQuery(): List<MethodParam> {
-        return params.filter { it.hasAnnotation(QUERY::class.java) }
-    }
-
-    fun getField(): List<MethodParam> {
-        return params.filter { it.hasAnnotation(FIELD::class.java) }
-    }
-
-    fun getMethod(): Annotation {
-        val method = annotations.filter { it is POST || it is GET }
-        check(method.size == 1) { "Can and only have one request method. " }
-        return method[0]
-    }
-
-    fun isGet(): Boolean {
-        return getMethod() is GET
-    }
-
-    fun isPost(): Boolean {
-        return getMethod() is POST
-    }
-
-
-    fun getPath(): String {
-        return when(val method = getMethod()) {
-            is GET -> method.path
-            is POST -> method.path
-            else -> throw IllegalStateException("Can and only have one request method. ")
-        }
-    }
-
-    fun parseReturnRawType(): Type {
-        return if (returnType is ParameterizedType) {
-            returnType.rawType
-        } else {
-            returnType
-        }
-    }
-
-    fun buildRequest(rootUrl: String): Request {
-        val query  = getQuery().map { it.annotation(QUERY::class.java).key to it.value }.toMap()
-        val headers = mutableMapOf<String, String>()
-        getHeaders().associateTo(headers) { it.annotation(HEADER::class.java).key to it.value }
-        val url = packageGetParam(rootUrl + getPath(), query)
-
-        Log.i(ThyiEx.TAG, "the url: $url")
-        var refer = ""
-
-        try {
-            val aURL = URL(url)
-            refer = aURL.protocol + "://" + aURL.host
-        } catch (ex: Exception) {
-            ex.printStackTrace()
-        }
-        headers["referer"] = refer
-        headers["user-agent"] = "Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Mobile Safari/537.36"
-
-
-        val rb = Request.Builder()
-                .url(url)
-                .headers(Headers.of(headers))
-
-
-        if (isPost()) {
-            val postBuilder = FormBody.Builder()
-            this.getField().forEach {
-                postBuilder.add(it.annotation(FIELD::class.java).key, it.value)
-            }
-            rb.post(postBuilder.build())
-        }
-
-        return rb.build()
-    }
-
-    private fun packageGetParam(url: String, params: Map<String, String>?): String {
-        if (params == null || params.isEmpty()) return url
-
-        val sb = StringBuilder(url)
-        if (!url.contains("?")) {
-            sb.append("?")
-        }
-
-        return sb.toString() + params.map { "${it.key}=${it.value}" }.joinToString("&")
-    }
-
-}
-
+//
+//import android.util.Log
+//import com.th.thyi.Thyi
+//import com.th.thyi.annotation.*
+//import okhttp3.FormBody
+//import okhttp3.Headers
+//import okhttp3.Request
+//import java.lang.IllegalStateException
+//import java.lang.reflect.Method
+//import java.lang.reflect.ParameterizedType
+//import java.lang.reflect.Type
+//import java.net.URL
+//
+///**
+// * api method为
+// */
+//data class ApiMethod(val method: Method, val args: (Array<Any?>)?) {
+//    val returnType: Type = method.genericReturnType
+//    val params: List<MethodParam> = method.parameterAnnotations.mapIndexed { index, annotations ->
+//        MethodParam(annotations.toList(), (args!![index] as String?)
+//                ?: "")
+//    }
+//    val annotations: List<Annotation> = method.annotations.toList()
+//
+//    init {
+//        println(returnType)
+//        require(returnType is ParameterizedType) { "method ${method.name} not return Observable" }
+//    }
+//
+//
+//    fun getHeaders(): List<MethodParam> {
+//        return params.filter { it.hasAnnotation(HEADER::class.java) }
+//    }
+//
+//    fun getQuery(): List<MethodParam> {
+//        return params.filter { it.hasAnnotation(QUERY::class.java) }
+//    }
+//
+//    fun getField(): List<MethodParam> {
+//        return params.filter { it.hasAnnotation(FIELD::class.java) }
+//    }
+//
+//    fun getMethod(): Annotation {
+//        val method = annotations.filter { it is POST || it is GET }
+//        check(method.size == 1) { "Can and only have one request method. " }
+//        return method[0]
+//    }
+//
+//    fun isGet(): Boolean {
+//        return getMethod() is GET
+//    }
+//
+//    fun isPost(): Boolean {
+//        return getMethod() is POST
+//    }
+//
+//
+//    fun getPath(): String {
+//        return when(val method = getMethod()) {
+//            is GET -> method.path
+//            is POST -> method.path
+//            else -> throw IllegalStateException("Can and only have one request method. ")
+//        }
+//    }
+//
+//    fun parseReturnRawType(): Type {
+//        return if (returnType is ParameterizedType) {
+//            returnType.rawType
+//        } else {
+//            returnType
+//        }
+//    }
+//
+//    fun buildRequest(rootUrl: String): Request {
+//        val query  = getQuery().map { it.annotation(QUERY::class.java).key to it.value }.toMap()
+//        val headers = mutableMapOf<String, String>()
+//        getHeaders().associateTo(headers) { it.annotation(HEADER::class.java).key to it.value }
+//        val url = packageGetParam(rootUrl + getPath(), query)
+//
+//        Log.i(Thyi.TAG, "the url: $url")
+//        var refer = ""
+//
+//        try {
+//            val aURL = URL(url)
+//            refer = aURL.protocol + "://" + aURL.host
+//        } catch (ex: Exception) {
+//            ex.printStackTrace()
+//        }
+//        headers["referer"] = refer
+//        headers["user-agent"] = "Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Mobile Safari/537.36"
+//
+//
+//        val rb = Request.Builder()
+//                .url(url)
+//                .headers(Headers.of(headers))
+//
+//
+//        if (isPost()) {
+//            val postBuilder = FormBody.Builder()
+//            this.getField().forEach {
+//                postBuilder.add(it.annotation(FIELD::class.java).key, it.value)
+//            }
+//            rb.post(postBuilder.build())
+//        }
+//
+//        return rb.build()
+//    }
+//
+//    private fun packageGetParam(url: String, params: Map<String, String>?): String {
+//        if (params == null || params.isEmpty()) return url
+//
+//        val sb = StringBuilder(url)
+//        if (!url.contains("?")) {
+//            sb.append("?")
+//        }
+//
+//        return sb.toString() + params.map { "${it.key}=${it.value}" }.joinToString("&")
+//    }
+//
+//}
+//
